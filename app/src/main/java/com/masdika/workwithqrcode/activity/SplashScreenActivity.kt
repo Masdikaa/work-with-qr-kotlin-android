c05050dd0dffd020f03345cc2c086700c2b139ce
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-24 07:58:21 +0700

    Work on Splash Screen

--- app/src/main/java/com/masdika/workwithqrcode/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/masdika/workwithqrcode/activity/SplashScreenActivity.kt
@@ -1,12 +1,37 @@
 package com.masdika.workwithqrcode.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import com.masdika.workwithqrcode.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         setContentView(R.layout.activity_splash_screen)
+
+        //Set Background Animation
+        val bgImage: ImageView = findViewById(R.id.splash_image)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        bgImage.startAnimation(slideAnimation)
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            // AVOIDING DOUBLE INSTANCE OF ACTIVITY
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish()
+        }, 2000)
+
     }
 }
