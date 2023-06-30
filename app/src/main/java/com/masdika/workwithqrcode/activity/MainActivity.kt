1f1eee81668cef636b9c8f1e9d0c234bd7305cf4
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-30 13:51:37 +0700

    Keyboard Listener in GeneratorLayout

--- app/src/main/java/com/masdika/workwithqrcode/activity/MainActivity.kt
+++ app/src/main/java/com/masdika/workwithqrcode/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.masdika.workwithqrcode.activity
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.masdika.workwithqrcode.R
 import com.masdika.workwithqrcode.databinding.ActivityMainBinding
@@ -17,6 +19,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val contentView = findViewById<View>(androidx.appcompat.R.id.content)
+        //KeyboardAppearListener
+        var isKeyboardShowing = false
+        fun onKeyboardVisibilityChanged(opened: Boolean) {
+            Log.d("Keyboard", "$opened")
+            if (opened) {
+                binding.bottomBar.visibility = View.INVISIBLE
+            } else {
+                binding.bottomBar.visibility = View.VISIBLE
+            }
+        }
+        // ContentView is the root view of the layout of this activity/fragment
+        contentView.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            contentView.getWindowVisibleDisplayFrame(r)
+            val screenHeight = contentView.rootView.height
+
+            // r.bottom is the position above soft keypad or device button.
+            // if keypad is shown, the r.bottom is smaller than that before.
+            val keypadHeight = screenHeight - r.bottom
+            Log.d("KeypadHeight", "$keypadHeight")
+
+            if (keypadHeight > screenHeight * 0.15) { // 0.15 ratio is perhaps enough to determine keypad height.
+                // keyboard is opened
+                if (!isKeyboardShowing) {
+                    isKeyboardShowing = true
+                    onKeyboardVisibilityChanged(true)
+                }
+            } else {
+                // keyboard is closed
+                if (isKeyboardShowing) {
+                    isKeyboardShowing = false
+                    onKeyboardVisibilityChanged(false)
+                }
+            }
+        }
 
         //Initiate first fragment
         supportFragmentManager.beginTransaction()
